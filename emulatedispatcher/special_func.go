6e3922315c9627833a50b32504a725e5831de10d
rynsf <rynsf@users.noreply.github.com>
2025-01-31 00:32:15 +0530

    add special function handler

--- emulatedispatcher/special_func.go
+++ emulatedispatcher/special_func.go
@@ -4,8 +4,28 @@ import (
 	driver "github.com/rynsf/nokstalgia/driver"
 )
 
+var specialFunc = make(map[uint32]func(*CpuState))
+
+/*
+Special functions are low level operating system functions.
+As this emulator emulates both hardware and the operating
+system of the phone, it handles very low level function
+separately. This function checks if the pc points to one such
+special function using a pre-initalize map and calls that
+function.
+*/
+func (s *CpuState) specialFuncHandler() bool {
+	f, ok := specialFunc[s.register[pc]]
+	if ok {
+		f(s)
+		s.register[pc] = s.register[lr]
+		return true
+	}
+	return false
+}
+
 // malloc is a wrapper to driver.malloc. It takes in the parameter for malloc from the register and set registers for output.
-func (s *CpuState) malloc() {
+func malloc(s *CpuState) {
 	size := s.register[0]
 	ptr := driver.Malloc(size)
 	ptr += s.dynRamBase
@@ -13,7 +33,7 @@ func (s *CpuState) malloc() {
 }
 
 // free is a wrapper for driver.free
-func (s *CpuState) free() {
+func free(s *CpuState) {
 	ptr := s.register[0]
 	ptr -= s.dynRamBase
 	driver.Free(ptr)
