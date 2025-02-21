6f446d304cd9edb97d8e8d39202aa22d6f7fa866
rynsf <rynsf@users.noreply.github.com>
2025-02-21 19:39:46 +0530

    add locate

---
+++ driver/locate.go
@@ -0,0 +1,25 @@
+package driver
+
+import (
+	"log"
+)
+
+var located = map[string]uint32{
+	"MALLOC":     0x2551F4,
+	"MARK_DIRTY": 0x2ADB0C,
+	"WIN_PRINT":  0x234B7A,
+	"MSG_ID":     0x10A4EA,
+	"MSG_ARGC":   0x10A4E6,
+	"MSG_ARGV":   0x10A4EC,
+}
+
+// First and a very basic implementation of locate function.
+// locate takes in name of an object and returns its memory address.
+func Locate(id string) uint32 {
+	addr, ok := located[id]
+	if ok {
+		return addr
+	}
+	log.Println("Locate: object not found")
+	return 0
+}
