24d43e363231eccac3686005ce6fa13a01bd3e2a
rynsf <rynsf@users.noreply.github.com>
2025-04-28 17:26:18 +0530

    add addresses to locate

--- driver/locate.go
+++ driver/locate.go
@@ -10,7 +10,45 @@ const (
 	MEM_MCU = 0x200000
 )
 
-var located = map[string]uint32{}
+// predefined addresses for now.
+// TODO: write locate functions for these.
+var located = map[string]uint32{
+	"SEND_MESSAGE":                  0x2F227E,
+	"BLOCK_ALLOC":                   0x2ACD14,
+	"BLOCK_DEALLOC":                 0x2ACCA4,
+	"BLOCK_ALLOC_NOWAIT":            0x2ACD94,
+	"MALLOC":                        0x2F9166,
+	"FREE":                          0x2F9300,
+	"DEV_FUNC_TRACE":                0x307C04,
+	"TI_ID_SEND":                    0x301E4C,
+	"OS_CONDITIONAL_INT_ENABLE":     0x2FA834,
+	"OS_CONDITIONAL_INT_DISABLE":    0x2FA812,
+	"DEV_DISP_REFRESH":              0x2F9A0E,
+	"SCREEN_BUFFER":                 0x140012,
+	"SCREEN_HEIGHT":                 0x41,
+	"SCREEN_WIDTH":                  0x60,
+	"OWN_TIMER_START":               0x3D43F0,
+	"LOAD_GLOBAL_SETTINGS_VALUE":    0x2CECE4,
+	"READ_DIRECTORY_FILE":           0x2F5D82,
+	"ENGINE_INITILIZE_FILE":         0x2CF114,
+	"TRANSFER_DOWNLOADED_DATA_INFO": 0x2CE72C,
+	"WRITE_DIRECTORY_FILE":          0x2CE72C,
+	"FREE_DIRECTORY_FILE":           0x2F610C,
+	"CREATE_MENU":                   0x307546,
+	"LOAD_SAVED_GAME":               0x2E5DB8,
+	"TRANSLATE_UCS2":                0x2BF8FE,
+	"OS_TIMER_STOP":                 0x2ABA4C,
+	"OS_TIMER_START":                0x2ABBD0,
+	"IND_CALL":                      0x308880,
+	"MSG_ID":                        0x10D9FA,
+	"MSG_ARGC":                      0x10D9F4,
+	"MSG_ARGV":                      0x10DA0C,
+	"DEV_KEY_GAME_MODE_ENABLE":      0x300FC8,
+	"TONE_CLASS_DISABLE":            0x301CF6,
+	"GAME_LOAD_HIGHSCORE":           0x2F4D98,
+	"ENGINE_LOAD_SETTINGS_VALUE":    0x2CEBA0,
+}
+
 var flash []byte
 
 func InitLocate(f []byte) {
@@ -69,7 +107,7 @@ func Locate(id string) uint32 {
 	if loc != 0 {
 		return handle(loc)
 	}
-	log.Println("Locate: object not found")
+	log.Printf("Locate: object %s not found\n", id)
 	return 0
 }
 
